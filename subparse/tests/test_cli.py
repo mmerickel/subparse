b426bba702e3396511ea8b4cae6c541ec9004fae
Michael Merickel <michael@merickel.org>
2013-08-05 22:11:32 -0500

    add test for load_commands

--- subparse/tests/test_cli.py
+++ subparse/tests/test_cli.py
@@ -1,4 +1,6 @@
-def test_it():
+import pytest
+
+def test_basic():
     from subparse import CLI
 
     cli = CLI()
@@ -20,3 +22,24 @@ def test_it():
 
 def foo_main(app, args):
     app['bar'] = args.bar
+
+def test_basic_decorator_dict():
+    from subparse import CLI, subcommand
+
+    @subcommand('subparse.tests.test_cli:foo_main')
+    def foo(parser):
+        """
+        short help
+        """
+        parser.add_argument('--bar', action='store_true')
+
+    cli = CLI()
+    cli.load_commands(locals())
+
+    args = cli.parse(['foo', '--bar'])
+    assert args.bar is True
+
+    app = {}
+    result = cli.dispatch(args, context=app)
+    assert app['bar'] is True
+    assert result == 0
